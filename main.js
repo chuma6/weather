aed5dc86a70340de1ef35b7cb3b0ee6a9b400058
Ismail <73080610+chuma6@users.noreply.github.com>
2021-02-21 13:04:30 +0100

    Update main.js

--- main.js
+++ main.js
@@ -24,10 +24,9 @@ window.addEventListener("load",function(){
     setInterval(function(){
         var date = new Date();
         var hour = `${date.getHours()}:${date.getMinutes()}`;
-        var getDay = date.getDay();
+        var getDay = 0;
         if(getDay == 0){
             timeDOM.innerHTML = timeDOM.innerHTML = `${hour},<span>Sunday</span>`
-
         }else{
             var day = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
             timeDOM.innerHTML = `${hour},<span> ${day[getDay-1]}</span>`
